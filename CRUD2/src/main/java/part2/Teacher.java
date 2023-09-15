112c34d106920283a4376d6e0d06cc5e6298a336
jnatasa <141272409+jnatasa@users.noreply.github.com>
2023-09-15 11:46:24 +0200

    Add files via upload

---
+++ CRUD2/src/main/java/part2/Teacher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package part2;
+
+/**
+ *
+ * @author integ
+ */
+public class Teacher {
+    
+  private String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
